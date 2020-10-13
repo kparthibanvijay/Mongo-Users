9d75679267f50ce120ddfef9e77dfb9e6bdb6266
PK <kparthibanvijay@gmail.com>
2020-10-12 21:59:13 -0400

    second commit. before making before block

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost:33017/user_test');
 
 mongoose.connection
-    .once('open', () => console.log('Good to go!'))
+    .once('open', () => {})
     .on('error', (error) => {
         console.warn('warning', error);
     });
+
+
+beforeEach((done) => {
+    mongoose.connection.collections.users.drop(()=>{
+        // ready to run the next test
+        done();
+    });
+});
