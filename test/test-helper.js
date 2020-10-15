7440f289489302390a0db6fe43ee5f1ae810b3aa
PK <kparthibanvijay@gmail.com>
2020-10-14 20:50:14 -0400

    more changes

--- test/test-helper.js
+++ test/test-helper.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 
-before((done)=>{
+before((done) => {
     mongoose.connect('mongodb://localhost:33017/user_test');
 
     mongoose.connection
@@ -12,13 +12,13 @@ before((done)=>{
         .on('error', (error) => {
             console.warn('warning', error);
         });
-    
+
 });
 
 
 
 beforeEach((done) => {
-    mongoose.connection.collections.users.drop(()=>{
+    mongoose.connection.collections.users.drop(() => {
         // ready to run the next test
         done();
     });
